7d2d0545cf1d8600ce44b2ee5b37d38c79f55f1a
AfsanehSharafi <Ashdev.ir@gmail.com>
2024-09-23 23:07:05 +0330

    Init Solution

---
+++ ShopManagement.Domain/ProductCategoryAgg/IProductCategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using ShopManagement.Domain.ProductCategoryAgg;
+
+namespace _0_Framework.Domain
+{
+    public interface IProductCategoryRepository
+    {
+        void Create(ProductCategory entity);
+
+        ProductCategory Get(long id);
+        List<ProductCategory> GetAll();
+    }
+}
